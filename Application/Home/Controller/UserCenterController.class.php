ad870a7f5eab412c4fea3e5df48301305c8ac4ba
Administrator <Administrator@yuan-PC>
2016-08-28 19:04:54 +0800

    tos

--- Application/Home/Controller/UserCenterController.class.php
+++ Application/Home/Controller/UserCenterController.class.php
@@ -25,9 +25,8 @@ class UserCenterController extends HomeController{
         $this->display();
     }
     public function delete(){
-        $data['id'] = 10;
+        $data['id'] = $_GET['id'];
         //$data['title'] = '哈哈测试';
         var_dump(delete('Yuanqu',$data));
-        echo  M("Yuanqu")->getLastSql();
     }
 }
